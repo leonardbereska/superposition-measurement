2dff0c398d8ee188ae8fb9fb80951a8e9ecabc7a
Leonard Bereska <leonard.bereska@zi-mannheim.de>
2025-05-14 11:53:57 +0200

    conda_env

--- setup_conda_env.sh
+++ setup_conda_env.sh
@@ -7,20 +7,4 @@ conda create -n superposition python=3.10 -y
 # Activate the environment
 conda activate superposition
 
-# Install PyTorch with CUDA support
-# Modify cuda version as needed (11.8, 12.1, etc.)
-# conda install pytorch torchvision pytorch-cuda=12.1 -c pytorch -c nvidia -y
-conda install pytorch torchvision -y  # for cpu
-
-# Install core data science packages
-conda install numpy matplotlib tqdm ipykernel jupyter -y
-
-# Install Hugging Face datasets
-pip install datasets
-
-# Install nnsight
-pip install nnsight
-
-# Install dictionary learning
-pip install dictionary-learning
 
