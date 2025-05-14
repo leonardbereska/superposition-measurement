52716cfe550309080a90de6a0c28e0c2f55afd7a
Leonard Bereska <leonard.bereska@zi-mannheim.de>
2025-05-14 11:53:11 +0200

    conda_env

---
+++ install_requirements.sh
@@ -0,0 +1,16 @@
+# Install PyTorch with CUDA support
+# Modify cuda version as needed (11.8, 12.1, etc.)
+# conda install pytorch torchvision pytorch-cuda=12.1 -c pytorch -c nvidia -y
+conda install pytorch torchvision -y  # for cpu
+
+# Install core data science packages
+conda install numpy matplotlib tqdm ipykernel jupyter -y
+
+# Install Hugging Face datasets
+pip install datasets
+
+# Install nnsight
+pip install nnsight
+
+# Install dictionary learning
+pip install dictionary-learning
